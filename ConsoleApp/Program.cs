a32c443e2371f27157111dbd619e8c4a8b919dbd
Rafael Bergo <rafaelbergo503@gmail.com>
2025-01-15 23:43:40 -0300

    Add MongoDB support with CRUD operations in ConsoleApp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MongoDB.Driver;
+
+string connectionString = "mongodb://localhost:27017"; 
+
+var client = new MongoClient(connectionString);
+
+while(true)
+{
+    Console.Clear();
+    Console.WriteLine("\nCRUD MongoDB:");
+    Console.WriteLine("1. Create Collection");
+    Console.WriteLine("2. List Collections");
+    Console.WriteLine("3. Delete Collection");
+    Console.WriteLine("4. Insert Document");
+    Console.WriteLine("5. List Documents");
+    Console.WriteLine("6. Edit Document");
+    Console.WriteLine("7. Delete Document");
+    Console.WriteLine("8. Exit");
+    Console.Write("Choose an option: ");
+    
+    var opcao = Console.ReadLine();
+
+    switch(opcao)
+    {
+        case "1":
+            CreateCollection(client);
+            break;
+        case "2":
+            ListCollections(client);
+            break;
+        case "3":
+            DeleteCollection(client);
+            break;
+        case "4":
+            InsertDocument(client);
+            break;
+        case "5":
+            ListDocuments(client);
+            break;
+        case "6":
+            EditDocument(client);
+            break;
+        case "7":
+            DeleteDocument(client);
+            break;
+        case "8":
+            return;
+        default:
+            Console.WriteLine("Invalid option");
+            break;
+    }
+}
+
+static void CreateCollection(MongoClient client)
+{
+    throw new NotImplementedException();
+}
+
+static void ListCollections(MongoClient client)
+{
+    throw new NotImplementedException();
+}
+
+static void DeleteCollection(MongoClient client)
+{
+    throw new NotImplementedException();
+}
+
+static void InsertDocument(MongoClient client)
+{
+    throw new NotImplementedException();
+}
+
+static void ListDocuments(MongoClient client)
+{
+    throw new NotImplementedException();
+}
+
+static void EditDocument(MongoClient client)
+{
+    throw new NotImplementedException();
+}
+
+static void DeleteDocument(MongoClient client)
+{
+    throw new NotImplementedException();
+}
